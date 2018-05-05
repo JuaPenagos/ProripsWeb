a6a424a5061e9c24a9f3fd55cbd397a55bcc6fd5
juaPenagos <juanpenagoss@hotmail.com>
2018-05-05 17:45:33 -0500

    inicio del proyecto web

---
+++ Prorips_Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Prorips_Web.Startup))]
+namespace Prorips_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
